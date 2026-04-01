f8713372ed1be4e16f8af8f2c3b2c19d8f09f4db
troyrob1-hash <troyrob1@gmail.com>
2026-04-01 12:48:43 -0700

    fix: wrap app in ToastProvider

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ export default function App() {
   useEffect(() => { init() }, [init])
 
   return (
-    <Routes>
+    <ToastProvider><OfflineBanner/><Routes>
       {/* Public auth routes */}
       <Route path="/login"  element={<PublicRoute><LoginPage /></PublicRoute>} />
       <Route path="/signup" element={<PublicRoute><SignUpPage /></PublicRoute>} />
@@ -84,6 +84,6 @@ export default function App() {
         } />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
-    </Routes>
+    </Routes></ToastProvider>
   )
 }
